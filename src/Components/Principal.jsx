6d3e5a90b1f1aa0fcf6a0238beef8f04bfafafef
Jhexaa <juan.zhou@utp.ac.pa>
2024-07-20 11:35:06 -0500

    pantalla principal

--- src/Components/Principal.jsx
+++ src/Components/Principal.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 
-const Principal = () => {
 
+const Principal = () => {
 
     return (
         <>
             <Navbar />
-            <h1>PRINCIPAL</h1>
+            <div className="cuerpo h-screen">
+                <div className="contenedor-menu">
+                    <div className="parrafo-menu">
+                        <h1>BIENVENIDO</h1>
+                        <p>¡Descubre el mundo a través de la ciencia con EduScience Quest!</p>
+                    </div>
+                    <div className="opciones-menu">
+                        <div class="ecosistema">
+                            <h2>Ecosistemas</h2>
+                            <img src="imagenes/logo-ecosistema.png" />
+                            <a href="menu-ecosistema.html"><button>Jugar</button></a>
+                        </div>
+                        <div class="animales">
+                            <h2>Animales</h2>
+                            <img src="imagenes/logo-animales.png" />
+                            <a href="menu-animales.html"><button>Jugar</button></a>
+                        </div>
+                        <div class="plantas">
+                            <h2>Plantas</h2>
+                            <img src="imagenes/logo-plantas.png" />
+                            <a href="menu-plantas.html"><button>Jugar</button></a>
+                        </div>
+                        <div class="agua">
+                            <h2>Ciclo del Agua</h2>
+                            <img src="imagenes/logo-cicloAgua.png" />
+                            <a href="menu-ciclo-agua.html"><button>Jugar</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
