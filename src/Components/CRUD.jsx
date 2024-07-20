60f7ced15f95e705d24d7fe8ed5ae67720b33daa
TheDavidFW <david.feng@utp.ac.pa>
2024-07-20 12:05:59 -0500

    Added crud.css and crud.jsx

---
+++ src/Components/CRUD.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+
+
+const CRUD = () => {
+
+    return (
+        <>
+            <Navbar />
+            
+        </>
+    )
+}
+export default CRUD
