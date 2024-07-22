c8ca9e90303e8a2110e6c8fb8e1225cbaadf72d4
Jhexaa <juan.zhou@utp.ac.pa>
2024-07-21 23:02:18 -0500

    asd

--- src/Components/Animales.jsx
+++ src/Components/Animales.jsx
@@ -24,7 +24,7 @@ const Animales = () => {
             const userChoice = window.confirm('Ya has completado este tema al 100%. ¿Quieres volver a jugar este tema?, perderás tu progreso');
             if (userChoice) {
                 resetGame(temaIdRequired, preguntaId);
-            } 
+            }
         } else if (game.tema_id >= temaIdRequired) {
             navigate(path);
         } else {
@@ -36,7 +36,7 @@ const Animales = () => {
         const userID = sessionStorage.getItem('userID');
         try {
             await apiService.updateGame(userID, tema_id, pregunta_id, 2);
-            setGame({ ...game, tema_id: tema_id, pregunta_id: pregunta_id});
+            setGame({ ...game, tema_id: tema_id, pregunta_id: pregunta_id });
             navigate('/pregunta-animales')
         } catch (error) {
             console.error('Error resetting game data:', error);
@@ -45,12 +45,12 @@ const Animales = () => {
 
     const getGame = async (id) => {
         try {
-          const response = await apiService.getGame(id);
-          setGame(response.data);
+            const response = await apiService.getGame(id);
+            setGame(response.data);
         } catch (error) {
-          console.error('Error fetching game data:', error);
+            console.error('Error fetching game data:', error);
         }
-      };
+    };
 
     useEffect(() => {
         const userID = sessionStorage.getItem('userID');
@@ -68,9 +68,12 @@ const Animales = () => {
                 <div className="volver-menu">
                     <button onClick={() => navigate('/principal')}>Volver a Menu Principal</button>
                 </div>
+                <div className="flex justify-center text-white text-5xl font-bold mb-20">
+                    <h1 className="px-8 bg-[#223021a8]">ANIMALES</h1>
+                </div>
                 <div className="opciones-menu-ecosistema">
-                    <div className="E-1" onClick={() => handleNavigation('/pregunta-animales', 4, progressE1, 13)}>
-                        <h3>Tipos de Animales</h3>
+                    <div className="E-1 px-10 gap-4" onClick={() => handleNavigation('/pregunta-animales', 4, progressE1, 13)}>
+                        <h3 className="text-2xl">Tipos de Animales</h3>
                         <img src={AnimalE1} alt="Animal E1" />
                         <div className="progress-container">
                             <div className="progress-bar" style={{ width: `${progressE1}%` }}>
@@ -78,8 +81,8 @@ const Animales = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="E-2" onClick={() => handleNavigation('/pregunta-animales', 5, progressE2, 17)}>
-                        <h3>Habitats</h3>
+                    <div className="E-2 px-10 gap-4" onClick={() => handleNavigation('/pregunta-animales', 5, progressE2, 17)}>
+                        <h3 className="text-2xl">Habitats</h3>
                         <img src={AnimalE2} alt="Animal E2" />
                         <div className="progress-container">
                             <div className="progress-bar" style={{ width: `${progressE2}%` }}>
@@ -87,8 +90,8 @@ const Animales = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="E-3" onClick={() => handleNavigation('/pregunta-animales', 6, progressE3, 21)}>
-                        <h3>Importancia de los animales</h3>
+                    <div className="E-3 px-10 gap-4" onClick={() => handleNavigation('/pregunta-animales', 6, progressE3, 21)}>
+                        <h3 className="text-2xl">Importancia de los animales</h3>
                         <img src={AnimalE3} alt="Animal E3" />
                         <div className="progress-container">
                             <div className="progress-bar" style={{ width: `${progressE3}%` }}>
